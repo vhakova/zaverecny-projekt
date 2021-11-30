a5e8365a5f1ffef2abfc44156e1dd41934f04ed4
Veronika Háková <veronika.hakova@gmail.com>
2021-11-30 17:31:20 +0100

    eleventy

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPassthroughCopy("images");
+	eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
